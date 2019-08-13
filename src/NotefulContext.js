1829b241c46ed6cea3c033fc0317207b37b7fdc1
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-13 11:31:29 -0400

    Edited Noteful Context to do the fetch

--- src/NotefulContext.js
+++ src/NotefulContext.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import store from './store';
 
-const NotefulContext = React.createContext({
-    data: store
-});
+let gfolders = [], gnotes = [];
+
+const setFolders = (folders) => {
+    gfolders = [...folders];
+    console.log(gfolders);
+}
+
+const setNotes = (notes) => {
+    gnotes = [...notes];
+    console.log(gnotes);
+}
+
+const getFoldersData = () => {
+
+    const fetchFolders = 'http://localhost:9090/folders';
+    const fetchNotes = 'http://localhost:9090/notes'
+    
+    let foldersData = [];
+
+    Promise.all([
+        fetch(fetchFolders),
+        fetch(fetchNotes)
+    ])
+    .then(([foldersRes, notesRes]) => {
+        if(!foldersRes.ok){
+            throw new Error('Folders where not found in the API');
+        }
+        if(!notesRes.ok){
+            throw new Error('Notes where not found in the API');
+        }
+
+        return (Promise.all([foldersRes.json(), notesRes.json()]));
+
+    })
+    .then(([folders, notes]) => {
+        const NotefulContext = React.createContext({
+            data: store,
+            folders : gfolders,
+            notes: gnotes
+        });
+    })
+    .catch((e) => {
+        console.log(e.message);
+    })
+};
+
+
 
 export default NotefulContext;
